0a59fa67daa2e2caa7882b0eee96c68b84b9b149
rcsio <78203730+rcsio@users.noreply.github.com>
2024-03-18 16:06:12 +0400

    Set up mockup api

---
+++ src/app/api/products/route.ts
@@ -0,0 +1,29 @@
+import { faker } from "@faker-js/faker";
+import { type NextRequest } from "next/server";
+import { randomUUID } from "node:crypto";
+
+export async function GET(request: NextRequest) {
+  const searchParams = request.nextUrl.searchParams;
+  const category = searchParams.get("category");
+
+  return Response.json([
+    {
+      id: randomUUID(),
+      name: faker.commerce.product(),
+      price: faker.commerce.price(),
+      img: `https://source.unsplash.com/random/?${category}`,
+    },
+    {
+      id: randomUUID(),
+      name: faker.commerce.product(),
+      price: faker.commerce.price(),
+      img: `https://source.unsplash.com/random/?${category}`,
+    },
+    {
+      id: randomUUID(),
+      name: faker.commerce.product(),
+      price: faker.commerce.price(),
+      img: `https://source.unsplash.com/random/?${category}`,
+    },
+  ]);
+}
