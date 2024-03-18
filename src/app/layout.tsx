91a0f35fb0525db55b7084b28d0cddc33d4083d0
rcsio <78203730+rcsio@users.noreply.github.com>
2024-03-18 09:40:04 +0400

    Add nav

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import Nav from "@/components/nav";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { ReactNode } from "react";
@@ -18,7 +19,9 @@ export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang="en">
       <body className={`${inter.className} min-h-dvh bg-stone-100`}>
-        <header></header>
+        <header className="sticky top-0">
+          <Nav />
+        </header>
         <main>{children}</main>
         <footer></footer>
       </body>
---
+++ src/components/nav.tsx
@@ -0,0 +1,38 @@
+import Logo from "@/assets/logo.png";
+import { Squares2X2Icon } from "@heroicons/react/24/outline";
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Nav() {
+  return (
+    <nav>
+      <ul className="flex items-center justify-between border-b border-b-stone-200 bg-white p-4">
+        <li>
+          <Link href="/">
+            <span className="sr-only">Home</span>
+            <Image src={Logo} alt="Logo of Rides 'n' Digits" />
+          </Link>
+        </li>
+        <div className="flex items-center gap-x-2">
+          <li>
+            <Link
+              href="/new"
+              className="inline-flex min-h-10 items-center rounded-full bg-orange-500 px-4 font-bold text-white"
+            >
+              Place Ad
+            </Link>
+          </li>
+          <li>
+            <Link
+              href="/"
+              className="inline-flex aspect-square min-h-10 items-center justify-center"
+            >
+              <span className="sr-only">Home</span>
+              <Squares2X2Icon className="h-6 w-6 text-orange-500" />
+            </Link>
+          </li>
+        </div>
+      </ul>
+    </nav>
+  );
+}
