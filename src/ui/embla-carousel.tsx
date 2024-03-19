27f850e54bdc162e710ee632b5daab9fb17b07ee
rcsio <78203730+rcsio@users.noreply.github.com>
2024-03-19 17:40:56 +0400

    Add optional dots prop to embla carousel

--- src/ui/embla-carousel.tsx
+++ src/ui/embla-carousel.tsx
@@ -1,22 +1,54 @@
 "use client";
 
+import clsx from "clsx";
+import { EmblaOptionsType } from "embla-carousel";
 import useEmblaCarousel from "embla-carousel-react";
-// @ts-ignore
-import { OptionsType } from "embla-carousel/components/Options";
-import { ReactNode } from "react";
+import { ReactNode, useEffect, useState } from "react";
 
 type Props = {
   className?: string;
   children: ReactNode;
-  options?: Partial<OptionsType>;
+  dots?: number;
+  options?: Partial<EmblaOptionsType>;
 };
 
-export default function EmblaCarousel({ className, children, options }: Props) {
-  const [emblaRef] = useEmblaCarousel(options);
+export default function EmblaCarousel({
+  className,
+  children,
+  dots,
+  options,
+}: Props) {
+  const [emblaRef, emblaApi] = useEmblaCarousel(options);
+  const [pos, setPos] = useState(0);
+
+  useEffect(() => {
+    if (emblaApi) {
+      emblaApi.on("select", () => setPos(emblaApi.selectedScrollSnap));
+    }
+  }, [emblaApi]);
 
   return (
     <div className={`overflow-hidden ${className}`} ref={emblaRef}>
       {children}
+
+      {dots && dots > 0 && (
+        <div className="mt-2 flex justify-center gap-x-2">
+          {[...Array(dots)].map((_, i) => (
+            <div
+              key={i}
+              className={clsx(
+                "aspect-square w-2 rounded-full",
+                {
+                  "border border-stone-800": i !== pos,
+                },
+                {
+                  "bg-stone-800": i === pos,
+                },
+              )}
+            />
+          ))}
+        </div>
+      )}
     </div>
   );
 }
