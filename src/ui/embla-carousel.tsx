b440a08e6b926f3d9264ba9a2557297e3e700df0
rcsio <78203730+rcsio@users.noreply.github.com>
2024-03-19 14:49:07 +0400

    Expose embla options

--- src/ui/embla-carousel.tsx
+++ src/ui/embla-carousel.tsx
@@ -1,15 +1,18 @@
 "use client";
 
 import useEmblaCarousel from "embla-carousel-react";
+// @ts-ignore
+import { OptionsType } from "embla-carousel/components/Options";
 import { ReactNode } from "react";
 
 type Props = {
   className?: string;
   children: ReactNode;
+  options?: Partial<OptionsType>;
 };
 
-export default function EmblaCarousel({ className, children }: Props) {
-  const [emblaRef] = useEmblaCarousel();
+export default function EmblaCarousel({ className, children, options }: Props) {
+  const [emblaRef] = useEmblaCarousel(options);
 
   return (
     <div className={`overflow-hidden ${className}`} ref={emblaRef}>
