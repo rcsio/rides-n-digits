77b3a710a0af5d6e72b47f1012f1b524cb74475f
rcsio <78203730+rcsio@users.noreply.github.com>
2024-03-28 16:07:17 +0400

    Create axios instance

---
+++ src/lib/axios.ts
@@ -0,0 +1,8 @@
+import axios from "axios";
+
+export default axios.create({
+  baseURL: "http://localhost:8000",
+  withCredentials: true,
+  withXSRFToken: true,
+  headers: { Accept: "application/json", "Content-Type": "application/json" },
+});
