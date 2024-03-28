3ecf1486aab3a0d4c35c236f0754bafa13b0c4e5
rcsio <78203730+rcsio@users.noreply.github.com>
2024-03-28 16:11:27 +0400

    Implement sign up functionality

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,38 +1,23 @@
-import Input from "@/ui/input";
+import Form from "@/app/signup/form";
 import Link from "next/link";
 
-export default function Signup() {
+export default function Login() {
   return (
-    <div className="mt-16 px-4">
-      <h1 className="grid place-items-center">
-        <span className="text-3xl font-bold">Sign up</span>{" "}
-        <span className="text-xl font-bold">for a better experience.</span>
+    <>
+      <h1 className="mx-4 mt-16 text-center">
+        <span className="text-3xl font-bold">Sign up</span>
+        <br />
+        <span className="text-xl font-bold"> for a better experience.</span>
       </h1>
 
-      <form action="" className="mt-12 grid gap-y-6">
-        <Input label="Email" name="email" type="email" required tabIndex={1} />
-        <Input
-          label="Password"
-          name="password"
-          type="password"
-          required
-          tabIndex={2}
-        />
-
-        <button
-          className="button bg-orange-500 font-bold text-white"
-          tabIndex={3}
-        >
-          Sign up
-        </button>
-      </form>
+      <Form />
 
       <p className="mt-6 text-center">
         Already have an account?{" "}
-        <Link href="/login" className="underline" tabIndex={4}>
+        <Link href="/login" className="underline" tabIndex={5}>
           Log in.
         </Link>
       </p>
-    </div>
+    </>
   );
 }
