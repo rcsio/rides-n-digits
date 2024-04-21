4c04d2ccb65d4b4c19f0188b346ae2f7a8f2c95b
rcsio <78203730+rcsio@users.noreply.github.com>
2024-04-21 18:19:27 +0400

    Extract component product

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,12 +1,7 @@
-import { type Product } from "@/types";
-import EmblaCarousel from "@/ui/embla-carousel";
+import { Product as ProductType } from "@/types";
 import Input from "@/ui/input";
-import dayjs from "dayjs";
-import relativeTime from "dayjs/plugin/relativeTime";
-import Image from "next/image";
+import Product from "@/ui/product";
 import Link from "next/link";
-
-dayjs.extend(relativeTime);
 
 type Props = {
   searchParams: { [key: string]: string | undefined };
@@ -15,7 +10,7 @@ type Props = {
 export default async function Search({ searchParams }: Props) {
   const url = `${process.env.NEXT_PUBLIC_API}/search?q=${searchParams.q}`;
   const res = await fetch(url);
-  const products: Product[] = await res.json();
+  const products: ProductType[] = await res.json();
 
   return (
     <div>
@@ -40,51 +35,13 @@ export default async function Search({ searchParams }: Props) {
         <ul className="my-4 space-y-2">
           {products.map((product) => (
             <li key={product.id}>
-              <Product data={product} />
-            </li>
-          ))}
-        </ul>
-      )}
-    </div>
-  );
-}
-
-function Product({ data }: { data: Product }) {
-  return (
-    <div className="flex flex-col-reverse">
-      <dl className="border-b border-b-stone-200 bg-white px-4">
-        <dt className="sr-only">Name</dt>
-        <dd className="mt-3 font-bold leading-6">{data.name}</dd>
-
-        <dt className="sr-only">Price</dt>
-        <dd className="mt-3 text-sm font-semibold leading-none">
-          AED {data.price}
-        </dd>
-
-        <dt className="sr-only">Posted</dt>
-        <dd className="mb-3.5 mt-1.5 text-sm leading-none">
-          Posted {dayjs(data.created_at).fromNow()}
-        </dd>
-      </dl>
-
-      <EmblaCarousel dots={data.images.length} options={{ loop: true }}>
-        <ul className="flex">
-          {data.images.map((img: string, i: number) => (
-            <li key={i} className="w-full shrink-0">
-              <Link href={`/ads/${data.id}`}>
-                <div className="relative block aspect-card">
-                  <Image
-                    src={img}
-                    alt={data.name}
-                    fill
-                    className="object-cover"
-                  />
-                </div>
+              <Link href={`/ads/${product.id}`}>
+                <Product data={product} />
               </Link>
             </li>
           ))}
         </ul>
-      </EmblaCarousel>
+      )}
     </div>
   );
 }
---
+++ src/ui/product.tsx
@@ -0,0 +1,53 @@
+import { type Product } from "@/types";
+import EmblaCarousel from "@/ui/embla-carousel";
+import dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime";
+import Image from "next/image";
+
+dayjs.extend(relativeTime);
+
+export default function Product({ data }: { data: Product }) {
+  return (
+    <div className="flex flex-col-reverse">
+      <dl className="border-b border-stone-200 bg-white px-4">
+        <div className="mt-3">
+          <dt className="sr-only">Name</dt>
+          <dd className="text-sm font-medium text-stone-600">{data.name}</dd>
+        </div>
+
+        <div className="mt-3">
+          <dt className="sr-only">Price</dt>
+          <dd className="font-semibold leading-none text-stone-700">
+            AED {data.price}
+          </dd>
+        </div>
+
+        <div className="mb-3 mt-1.5 flex items-center">
+          <dt className="mr-[0.15em] text-xs font-medium leading-none text-stone-500">
+            Posted
+          </dt>
+          <dd className="ml-[0.15em] text-xs font-medium leading-none text-stone-500">
+            {dayjs(data.created_at).fromNow()}
+          </dd>
+        </div>
+      </dl>
+
+      <EmblaCarousel dots={data.images.length} options={{ loop: true }}>
+        <ul className="flex">
+          {data.images.map((img, i) => (
+            <li key={i} className="w-full shrink-0">
+              <div className="relative block aspect-card">
+                <Image
+                  src={img}
+                  alt={data.name}
+                  fill
+                  className="object-cover"
+                />
+              </div>
+            </li>
+          ))}
+        </ul>
+      </EmblaCarousel>
+    </div>
+  );
+}
