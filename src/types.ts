096a1b5631624c733f886780faec06e86d42dd99
rcsio <78203730+rcsio@users.noreply.github.com>
2024-04-21 18:13:56 +0400

    New type: Category

--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,11 @@
+export type Category = {
+  id: number;
+  name: string;
+  slug: string;
+  created_at: string;
+  updated_at: string;
+};
+
 export type Product = {
   id: string;
   attributes: { [key: string]: string };
