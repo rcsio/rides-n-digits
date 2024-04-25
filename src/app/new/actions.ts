d844e8b781292b0ca8b5ab15cf4089f01258aed5
rcsio <78203730+rcsio@users.noreply.github.com>
2024-04-25 10:59:58 +0400

    Upload image on change

--- src/app/new/actions.ts
+++ src/app/new/actions.ts
@@ -1,12 +1,15 @@
 "use server";
 
 import { getUser } from "@/lib/auth";
+import axios from "axios";
 import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
 
 export async function createProduct(formData: FormData) {
   const user = await getUser();
   if (!user) redirect("/login");
+
+  const images = JSON.parse(cookies().get("images")?.value || "[]");
 
   const res = await fetch(`${process.env.BACKEND_URL}/api/products`, {
     body: JSON.stringify({
@@ -15,6 +18,7 @@ export async function createProduct(formData: FormData) {
       name: formData.get("name"),
       price: formData.get("price"),
       open_to_offers: !!formData.get("open_to_offers"),
+      images,
     }),
     headers: {
       Accept: "application/json",
@@ -24,5 +28,27 @@ export async function createProduct(formData: FormData) {
     method: "POST",
   });
 
-  if (res.ok) redirect("/dashboard");
+  if (res.ok) {
+    cookies().delete("images");
+    redirect("/dashboard");
+  }
+}
+
+export async function uploadImage(formData: FormData) {
+  const user = await getUser();
+  if (!user) redirect("/login");
+
+  const url = `${process.env.BACKEND_URL}/api/tmp`;
+  const { data } = await axios.post(url, formData, {
+    headers: { Authorization: `Bearer ${cookies().get("AUTH_TOKEN")?.value}` },
+  });
+
+  // const images = JSON.parse(cookies().get('images')?.value || '[]')
+  const images = [data];
+
+  cookies().set({
+    name: "images",
+    value: JSON.stringify(images),
+    secure: process.env.NODE_ENV === "production",
+  });
 }
--- src/app/new/ui/photos.tsx
+++ src/app/new/ui/photos.tsx
@@ -1,61 +1,40 @@
 "use client";
 
-import { XCircleIcon } from "@heroicons/react/16/solid";
+import { uploadImage } from "@/app/new/actions";
 import { ChangeEvent, useEffect, useState } from "react";
 
-const MAX_PHOTOS = 4;
-
 export default function Photos() {
-  const [files, setFiles] = useState<File[]>([]);
+  const [file, setFile] = useState<File>();
 
-  function handleChange(e: ChangeEvent<HTMLInputElement>) {
-    const _files = Array.from(e.currentTarget.files!);
+  async function handleChange(e: ChangeEvent<HTMLInputElement>) {
+    const file = e.currentTarget.files![0];
+    setFile(file);
 
-    const newFiles = _files.filter((_f) => {
-      return !files.some((f) => f.name === _f.name);
-    });
+    if (!file) return;
 
-    const remaining = MAX_PHOTOS - files.length;
+    const formData = new FormData();
+    formData.append("file", file);
 
-    if (remaining > 0) {
-      setFiles((prevState) => [...prevState, ...newFiles.slice(0, remaining)]);
-    }
+    await uploadImage(formData);
   }
 
   return (
     <>
-      {files.map((file, i) => (
-        <div key={i} className="relative w-full shrink-0">
-          {/* eslint-disable-next-line jsx-a11y/alt-text */}
-          <Image file={file} />
+      {/* eslint-disable-next-line jsx-a11y/alt-text */}
+      {file && <Image file={file} />}
 
-          <button
-            type="button"
-            className="absolute right-1 top-1 flex aspect-square min-h-10 items-center justify-center rounded-xl bg-white/50 backdrop-blur-sm"
-            onClick={() => {
-              setFiles(files.filter((_file) => _file.name !== file.name));
-            }}
-          >
-            <span className="sr-only">Remove</span>
-            <XCircleIcon className="h-4 w-4 text-red-600" />
-          </button>
-        </div>
-      ))}
-
-      {files.length < MAX_PHOTOS && (
-        <label className="button w-full cursor-pointer border border-blue-500 bg-white hover:bg-blue-100 has-[:focus]:bg-blue-100">
-          <input
-            type="file"
-            accept=".jpg, .jpeg"
-            name="photos"
-            multiple
-            className="sr-only"
-            tabIndex={6}
-            onChange={handleChange}
-          />
-          {files.length > 0 ? "Add more photos" : "Choose photos"}
-        </label>
-      )}
+      <label className="button w-full cursor-pointer border border-blue-500 bg-white hover:bg-blue-100 has-[:focus]:bg-blue-100">
+        <input
+          type="file"
+          accept=".jpg, .jpeg"
+          name="file"
+          className="sr-only"
+          tabIndex={6}
+          onChange={handleChange}
+          required
+        />
+        {file ? "Change image" : "Select image"}
+      </label>
     </>
   );
 }
