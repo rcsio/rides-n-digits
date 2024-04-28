5b64bf02d7fb8756f2aeac932e4f0bfeb42eaf6a
rcsio <78203730+rcsio@users.noreply.github.com>
2024-04-28 15:33:22 +0400

    New UI: Select

---
+++ src/ui/select.tsx
@@ -0,0 +1,33 @@
+import { camelCase } from "change-case";
+import clsx from "clsx";
+import { InputHTMLAttributes } from "react";
+
+interface Props extends InputHTMLAttributes<HTMLSelectElement> {
+  label: string;
+  name: string;
+  options: { label: string; value: string | number }[];
+  srLabel?: boolean;
+}
+
+export default function Select(props: Props) {
+  const { id, label, name, options, srLabel, ...rest } = props;
+  const _id = id || name || camelCase(label);
+
+  return (
+    <div className="grid gap-y-1">
+      <label
+        htmlFor="transmission"
+        className={clsx("justify-self-start", "sr-only" && srLabel)}
+      >
+        {label}
+      </label>
+      <select {...rest} name={name} id={_id} className="input">
+        {options.map((option, i) => (
+          <option key={i} value={option.value}>
+            {option.label}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+}
