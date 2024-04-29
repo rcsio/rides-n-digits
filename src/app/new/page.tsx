838428785b0189c562c3e91838fb3b3d4c076769
rcsio <78203730+rcsio@users.noreply.github.com>
2024-04-29 13:44:45 +0400

    Update create product page
    - Change Description to Short Description
    - Change 5 rows to 4
    - Remove tab indexes
    - Use slug as value of category

--- src/app/new/page.tsx
+++ src/app/new/page.tsx
@@ -1,13 +1,13 @@
 import { createProduct } from "@/app/new/actions";
+import Photos from "@/app/new/ui/photos";
 import SubmitButton from "@/app/new/ui/submit-button";
 import { Category } from "@/types";
 import Checkbox from "@/ui/checkbox";
 import Input from "@/ui/input";
+import Select from "@/ui/select";
 import Textarea from "@/ui/textarea";
 import { singular } from "pluralize";
 import title from "title";
-
-import Photos from "./ui/photos";
 
 export default async function Advertise() {
   const url = `${process.env.BACKEND_URL}/api/categories`;
@@ -20,46 +20,30 @@ export default async function Advertise() {
     <form className="space-y-6 px-4 py-6" action={createProduct}>
       <h1 className="text-xl font-bold">Place Ad</h1>
 
-      <div className="grid gap-y-1">
-        <label htmlFor="category" className="justify-self-start">
-          Category
-        </label>
-        <select name="category_id" id="category" className="input" tabIndex={1}>
-          {categories.map(({ id, name }) => (
-            <option key={id} value={id}>
-              {title(singular(name))}
-            </option>
-          ))}
-        </select>
-      </div>
+      <Select
+        label="Category"
+        name="category_slug"
+        options={categories.map(({ name, slug }) => ({
+          label: title(singular(name)),
+          value: slug,
+        }))}
+      />
 
-      <Input label="Name" name="name" required spellCheck tabIndex={2} />
+      <Input label="Ad Title" name="name" required spellCheck />
 
       <Textarea
-        label="Description"
+        label="Short Description"
         name="description"
         maxLength={1000}
-        rows={5}
+        rows={4}
         spellCheck
-        tabIndex={3}
         required
       />
 
-      <Input
-        label="Price"
-        name="price"
-        type="number"
-        min={1}
-        required
-        tabIndex={4}
-      />
-
-      <Checkbox name="open_to_offers" tabIndex={5}>
-        Open to price offers
-      </Checkbox>
+      <Input label="Price" name="price" type="number" min={1} required />
+      <Checkbox name="open_to_offers">Open to price offers</Checkbox>
 
       <Photos />
-
       <SubmitButton />
     </form>
   );
