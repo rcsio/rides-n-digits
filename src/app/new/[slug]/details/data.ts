ca496c47e5ce64c75adbe2be4f200605046cbb2d
rcsio <78203730+rcsio@users.noreply.github.com>
2024-05-05 11:26:42 +0400

    Update data for details

--- src/app/new/[slug]/details/data.ts
+++ src/app/new/[slug]/details/data.ts
@@ -1,4 +1,4 @@
-const regionalSpecs = [
+export const regionalSpecs = [
   "American",
   "Canadian",
   "Chinese",
@@ -9,9 +9,31 @@ const regionalSpecs = [
   "Other",
 ];
 
-export const car = {
-  regionalSpecs,
-};
+export const fuels = ["Diesel", "Electric", "Gasoline", "Hybrid"];
+
+export const transmissions = ["Automatic", "Manual"];
+
+export const horsePowers = [
+  "Below 150 hp",
+  "150-199 hp",
+  "200-299 hp",
+  "300-399 hp",
+  "400-499 hp",
+  "Above 500 hp",
+];
+
+export const lengths = [
+  "Under 10 ft",
+  "10-14 ft",
+  "15-19 ft",
+  "20-24 ft",
+  "25-29 ft",
+  "30-39 ft",
+  "40-49 ft",
+  "50-69 ft",
+  "70-100 ft",
+  "100+ ft",
+];
 
 export const motorcycle = {
   engineSizes: [
@@ -23,7 +45,6 @@ export const motorcycle = {
     "1,000cc or more",
   ],
   finalDriveSystems: ["Belt", "Chain", "Shaft", "Other"],
-  regionalSpecs,
   types: [
     "Adventure/Touring",
     "Cafe Racer",
@@ -36,6 +57,26 @@ export const motorcycle = {
     "Standard/Commuter",
     "Trailer",
     "Trike",
+    "Other",
+  ],
+};
+
+export const car = {
+  bodyTypes: [
+    "Coupe",
+    "Crossover",
+    "Fiber",
+    "Hard Top Convertible",
+    "Hatchback",
+    "Metal",
+    "Pick Up Truck",
+    "Sedan",
+    "Soft Top Convertible",
+    "Sports Car",
+    "SUV",
+    "Utility Truck",
+    "Van",
+    "Wagon",
     "Other",
   ],
 };
