dc34d1d943f20e41ff698f95fcca29cd33c75818
rcsio <78203730+rcsio@users.noreply.github.com>
2024-05-05 11:27:09 +0400

    Support other categories for entering details

--- src/app/new/[slug]/details/form.tsx
+++ src/app/new/[slug]/details/form.tsx
@@ -1,6 +1,14 @@
 import { tempSaveAttribs } from "@/app/new/[slug]/details/actions";
 import Button from "@/app/new/[slug]/details/button";
-import { motorcycle } from "@/app/new/[slug]/details/data";
+import {
+  car,
+  fuels,
+  horsePowers,
+  lengths,
+  motorcycle,
+  regionalSpecs,
+  transmissions,
+} from "@/app/new/[slug]/details/data";
 import { colors } from "@/data";
 import { Category } from "@/types";
 import Checkbox from "@/ui/checkbox";
@@ -73,7 +81,7 @@ export default function Form({ category }: { category: Category }) {
           <Select
             label="Regional Specs"
             name="regional_specs"
-            options={motorcycle.regionalSpecs.map(getLabelValue)}
+            options={regionalSpecs.map(getLabelValue)}
             defaultValue={attributes?.regional_specs}
           />
           <Checkbox
@@ -83,6 +91,326 @@ export default function Form({ category }: { category: Category }) {
           >
             Has Warranty
           </Checkbox>
+        </>
+      )}
+
+      {category.slug === "car" && (
+        <>
+          <Input
+            label="Make"
+            name="make"
+            defaultValue={attributes?.make}
+            required
+          />
+          <Input
+            label="Model"
+            name="model"
+            defaultValue={attributes?.model}
+            required
+          />
+          <Input
+            label="Year"
+            name="year"
+            type="number"
+            min={1900}
+            defaultValue={attributes?.year}
+            required
+          />
+          <Select
+            label="Color"
+            name="color"
+            options={colors.map(getLabelValue)}
+            defaultValue={attributes?.color}
+          />
+          <Input
+            label="Doors"
+            name="doors"
+            type="number"
+            min={1}
+            defaultValue={attributes?.doors}
+            required
+          />
+          <Select
+            label="Body Type"
+            name="body_type"
+            options={car.bodyTypes.map(getLabelValue)}
+            defaultValue={attributes?.body_type}
+          />
+          <Input
+            label="Kilometers"
+            name="kilometers"
+            type="number"
+            min={0}
+            defaultValue={attributes?.kilometers}
+            required
+          />
+          <Input
+            label="Cylinders"
+            name="cylinders"
+            type="number"
+            min={1}
+            defaultValue={attributes?.cylinders}
+            required
+          />
+          <Select
+            label="Transmission"
+            name="transmission"
+            options={transmissions.map(getLabelValue)}
+            defaultValue={attributes?.transmission}
+          />
+          <Select
+            label="Horsepower"
+            name="horsepower"
+            options={horsePowers.map(getLabelValue)}
+            defaultValue={attributes?.horsepower}
+          />
+          <Select
+            label="Fuel"
+            name="fuel"
+            options={fuels.map(getLabelValue)}
+            defaultValue={attributes?.fuel}
+          />
+          <Select
+            label="Regional Specs"
+            name="regional_specs"
+            options={regionalSpecs.map(getLabelValue)}
+            defaultValue={attributes?.regional_specs}
+          />
+          <Checkbox
+            name="warranty"
+            defaultChecked={attributes?.warranty}
+            value={1}
+          >
+            Has Warranty
+          </Checkbox>
+        </>
+      )}
+
+      {category.slug === "heavy-vehicle" && (
+        <>
+          <Input
+            label="Make"
+            name="make"
+            defaultValue={attributes?.make}
+            required
+          />
+          <Input
+            label="Model"
+            name="model"
+            defaultValue={attributes?.model}
+            required
+          />
+          <Input
+            label="Year"
+            name="year"
+            type="number"
+            min={1900}
+            defaultValue={attributes?.year}
+            required
+          />
+          <Select
+            label="Color"
+            name="color"
+            options={colors.map(getLabelValue)}
+            defaultValue={attributes?.color}
+          />
+          <Input
+            label="Kilometers"
+            name="kilometers"
+            type="number"
+            min={0}
+            defaultValue={attributes?.kilometers}
+            required
+          />
+          <Input
+            label="Cylinders"
+            name="cylinders"
+            type="number"
+            min={1}
+            defaultValue={attributes?.cylinders}
+            required
+          />
+          <Select
+            label="Horsepower"
+            name="horsepower"
+            options={horsePowers.map(getLabelValue)}
+            defaultValue={attributes?.horsepower}
+          />
+          <Select
+            label="Fuel"
+            name="fuel"
+            options={fuels.map(getLabelValue)}
+            defaultValue={attributes?.fuel}
+          />
+          <Select
+            label="Regional Specs"
+            name="regional_specs"
+            options={regionalSpecs.map(getLabelValue)}
+            defaultValue={attributes?.regional_specs}
+          />
+          <Checkbox
+            name="warranty"
+            defaultChecked={attributes?.warranty}
+            value={1}
+          >
+            Has Warranty
+          </Checkbox>
+        </>
+      )}
+
+      {category.slug === "bicycle" && (
+        <>
+          <Input
+            label="Make"
+            name="make"
+            defaultValue={attributes?.make}
+            required
+          />
+          <Input
+            label="Model"
+            name="model"
+            defaultValue={attributes?.model}
+            required
+          />
+          <Input
+            label="Year"
+            name="year"
+            type="number"
+            min={1900}
+            defaultValue={attributes?.year}
+            required
+          />
+          <Select
+            label="Color"
+            name="color"
+            options={colors.map(getLabelValue)}
+            defaultValue={attributes?.color}
+          />
+          <Checkbox
+            name="warranty"
+            defaultChecked={attributes?.warranty}
+            value={1}
+          >
+            Has Warranty
+          </Checkbox>
+        </>
+      )}
+
+      {category.slug === "boat" && (
+        <>
+          <Input
+            label="Make"
+            name="make"
+            defaultValue={attributes?.make}
+            required
+          />
+          <Input
+            label="Model"
+            name="model"
+            defaultValue={attributes?.model}
+            required
+          />
+          <Input
+            label="Year"
+            name="year"
+            type="number"
+            min={1900}
+            defaultValue={attributes?.year}
+            required
+          />
+          <Select
+            label="Color"
+            name="color"
+            options={colors.map(getLabelValue)}
+            defaultValue={attributes?.color}
+          />
+          <Select
+            label="Length"
+            name="length"
+            options={lengths.map(getLabelValue)}
+            defaultValue={attributes?.length}
+          />
+          <Checkbox
+            name="warranty"
+            defaultChecked={attributes?.warranty}
+            value={1}
+          >
+            Has Warranty
+          </Checkbox>
+        </>
+      )}
+
+      {category.slug === "airplane" && (
+        <>
+          <Input
+            label="Make"
+            name="make"
+            defaultValue={attributes?.make}
+            required
+          />
+          <Input
+            label="Model"
+            name="model"
+            defaultValue={attributes?.model}
+            required
+          />
+          <Input
+            label="Year"
+            name="year"
+            type="number"
+            min={1900}
+            defaultValue={attributes?.year}
+            required
+          />
+          <Select
+            label="Color"
+            name="color"
+            options={colors.map(getLabelValue)}
+            defaultValue={attributes?.color}
+          />
+          <Checkbox
+            name="warranty"
+            defaultChecked={attributes?.warranty}
+            value={1}
+          >
+            Has Warranty
+          </Checkbox>
+        </>
+      )}
+
+      {category.slug === "horse" && (
+        <>
+          <Input
+            label="Breed"
+            name="breed"
+            defaultValue={attributes?.breed}
+            required
+          />
+          <Input
+            label="Year Born"
+            name="year_born"
+            type="number"
+            min={1900}
+            defaultValue={attributes?.year_born}
+            required
+          />
+          <Select
+            label="Color"
+            name="color"
+            options={colors.map(getLabelValue)}
+            defaultValue={attributes?.color}
+          />
+        </>
+      )}
+
+      {category.slug === "license-plate" && (
+        <>
+          <Input
+            label="License Plate"
+            name="license_plate"
+            defaultValue={attributes?.license_plate}
+            required
+          />
         </>
       )}
 
