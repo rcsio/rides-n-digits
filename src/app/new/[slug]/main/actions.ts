4ed7925f8d8adebecb9608cd9e081e87a5cb4171
rcsio <78203730+rcsio@users.noreply.github.com>
2024-05-05 16:24:27 +0400

    Fix open_to_offers is required

--- src/app/new/[slug]/main/actions.ts
+++ src/app/new/[slug]/main/actions.ts
@@ -9,18 +9,20 @@ export async function createProduct(categorySlug: string, formData: FormData) {
   const user = await getUser();
   if (!user) redirect("/login");
 
+  const entries = Object.fromEntries(formData);
   const attributes = JSON.parse(cookies().get("attributes")?.value || "{}");
   const images = JSON.parse(cookies().get("images")?.value || "[]");
 
-  const entries = {
-    ...Object.fromEntries(formData),
-    category_slug: categorySlug,
-    attributes,
+  const payload = {
+    ...entries,
     images,
+    attributes,
+    category_slug: categorySlug,
+    open_to_offers: !!entries.open_to_offers,
   };
 
   const res = await fetch(`${process.env.BACKEND_URL}/api/products`, {
-    body: JSON.stringify(entries),
+    body: JSON.stringify(payload),
     headers: {
       Accept: "application/json",
       Authorization: `Bearer ${cookies().get("AUTH_TOKEN")?.value}`,
